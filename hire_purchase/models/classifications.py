a3dc1dac9edba05718cd4083d00ffad023ca6f77
akradore <akradore@gmail.com>
2020-05-23 15:04:14 +0200

    [DEL]

--- hire_purchase/models/classifications.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models, api, _
-class Classifications(models.Model):
-    
-    _name = 'hp.classifications'
-    _description = "HP classifications"
-    
-    name = fields.Char(string="Name",  required=True)
-    min = fields.Integer(string="Min Days in Arrears", required=True)
-    max = fields.Integer(string="Max Days in Arrears", required=True)
